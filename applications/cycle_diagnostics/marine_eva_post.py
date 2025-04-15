d33f8ff924844e65d128fc8f30b222a2518e668f
KatherineLukens-NOAA <59662895+KatherineLukens-NOAA@users.noreply.github.com>
2025-04-14 22:00:42 -0400

    Add extra EVA map_plots: OmA, hofx, ObsError, ObsTime (#517)
    * Add extra EVA map_plots: OmA, hofx, ObsError, ObsTime
    
    * Updates to marine_eva_post.py and requirements.txt

--- applications/cycle_diagnostics/marine_eva_post.py
+++ applications/cycle_diagnostics/marine_eva_post.py
@@ -5,15 +5,32 @@ import logging
 import os
 import socket
 import yaml
+import numpy as np
+from netCDF4 import Dataset
 
 # sets the cmap vmin/vmax for each variable
 # TODO: this should probably be in a yaml or something
+# ... For OmB, OmA
 vminmax = {'seaSurfaceTemperature': {'vmin': -2.0, 'vmax': 2.0},
            'seaIceFraction': {'vmin': -0.2, 'vmax': 0.2},
            'seaSurfaceSalinity': {'vmin': -0.2, 'vmax': 0.2},  # TODO: this should be changed
            'absoluteDynamicTopography': {'vmin': -0.2, 'vmax': 0.2},
            'waterTemperature': {'vmin': -2.0, 'vmax': 2.0},
            'salinity': {'vmin': -0.2, 'vmax': 0.2}}
+# ... For hofx
+vminmax_hofx = {'seaSurfaceTemperature': {'vmin': -5.0, 'vmax': 30.0},
+                'seaIceFraction': {'vmin': 0.0, 'vmax': 1.0},
+                'seaSurfaceSalinity': {'vmin': 0.0, 'vmax': 50.0},  # TODO: this should be changed
+                'absoluteDynamicTopography': {'vmin': 0.0, 'vmax': 1.0},
+                'waterTemperature': {'vmin': -5.0, 'vmax': 30.0},
+                'salinity': {'vmin': 0.0, 'vmax': 50.0}}
+# ... For obs errors
+vminmax_error = {'seaSurfaceTemperature': {'vmin': 0.0, 'vmax': 1.0},
+                 'seaIceFraction': {'vmin': 0.0, 'vmax': 0.2},
+                 'seaSurfaceSalinity': {'vmin': 0.0, 'vmax': 10.0},  # TODO: this should be changed
+                 'absoluteDynamicTopography': {'vmin': 0.0, 'vmax': 1.0},
+                 'waterTemperature': {'vmin': 0.0, 'vmax': 1.0},
+                 'salinity': {'vmin': 0.0, 'vmax': 10.0}}
 
 
 def marine_eva_post(inputyaml, outputdir, diagdir):
@@ -26,20 +43,53 @@ def marine_eva_post(inputyaml, outputdir, diagdir):
         logging.error(f'Error occurred when attempting to load: {inputyaml}, error: {e}')
         return
     for dataset in input_yaml_dict['datasets']:
+        # Get filenames
         newfilenames = []
         for filename in dataset['filenames']:
             newfilename = os.path.join(diagdir, os.path.basename(filename))
             newfilenames.append(newfilename)
         dataset['filenames'] = newfilenames
+
+    # Set vmin/vmax based on yaml and var_min/var_max
     for graphic in input_yaml_dict['graphics']['figure_list']:
-        # this assumes that there is only one variable, or that the
+        # This assumes that there is only one variable, or that the
         # variables are all the same
         variable = graphic['batch figure']['variables'][0]
         for plot in graphic['plots']:
             for layer in plot['layers']:
+                # Find plot type
+                plotdata = layer
+                typestr = str(plotdata)
                 if layer['type'] == 'MapScatter':
-                    layer['vmin'] = vminmax[variable]['vmin']
-                    layer['vmax'] = vminmax[variable]['vmax']
+                    # Find min/max obs time if plot is of "metadata"/"dateTime"
+                    if str(typestr).lower().find('obstime') != -1:
+                        # Open diag file to get "dateTime" variable
+                        ds = Dataset(dataset['filenames'][0], mode="r")
+                        vardata = ds.groups["MetaData"].variables["dateTime"]
+                        # Find min/max dateTime (in seconds from epoch)
+                        tmin = np.nanmin(vardata)
+                        tmax = np.nanmax(vardata)
+                        var_min = int(np.round(tmin, decimals=-1))
+                        var_max = int(np.round(tmax, decimals=-1))
+                        # Close diag file
+                        ds.close()
+                        break
+                    # Set min/max for map plot
+                    if typestr.lower().find('omb') != -1 or typestr.lower().find('oma') != -1:
+                        layer['vmin'] = vminmax[variable]['vmin']
+                        layer['vmax'] = vminmax[variable]['vmax']
+                    elif typestr.lower().find('hofx') != -1:
+                        layer['vmin'] = vminmax_hofx[variable]['vmin']
+                        layer['vmax'] = vminmax_hofx[variable]['vmax']
+                    elif typestr.lower().find('error') != -1:
+                        layer['vmin'] = vminmax_error[variable]['vmin']
+                        layer['vmax'] = vminmax_error[variable]['vmax']
+                    elif typestr.lower().find('obstime') != -1:
+                        layer['vmin'] = var_min
+                        layer['vmax'] = var_max
+                    else:
+                        layer['vmin'] = vminmax[variable]['vmin']
+                        layer['vmax'] = vminmax[variable]['vmax']
 
     # first, let us prepend some comments that tell someone this output YAML was generated
     now = datetime.datetime.now()
